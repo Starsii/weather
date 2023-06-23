74683c89d5b60c7153c723b1265901df9594a564
Starsii <estrellaanguiano99@gmail.com>
2023-06-23 13:51:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import Weather from './components/weather'
 
 function App() {
+
+  const [lat, setLat] = useState([])
+  const [long, setLong] = useState([])
+  const [data, setData] = useState([])
+  const [city, setCity] = useState([])
+  const key = 'cae900f3dff9e564570009fe5903ac15'
+
+  
+  useEffect(() => {
+    /////////////////////////////////////////call back function on success
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+
+      })
+
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=imperial`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result)
+      })
+    }
+    fetchData()
+    console.log("lat is", lat)
+    console.log("long is", long)
+  },[lat,long,])
+
+  useEffect(() => {
+    const fetchCity = async () => {
+      await fetch('https://api.radar.io/v1/geocode/reverse?coordinates=41.7852654,-87.6973214', {
+        headers: {
+          'Authorization' : 'prj_live_sk_62a5d1002fe62967aa211826d3a3c4c190fd4f88'
+        }
+      })
+      .then(res => res.json())
+      .then(result => {
+        setCity(result)
+        console.log(result)
+      })
+    }
+    fetchCity()
+  },[lat,long])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data} locationData={city}/>
+      ): (
+        <div></div>
+      )}
+   </div>
   );
 }
 
 export default App;
+
+//  useEffect(() => {
+//     const fetchData = async () => {
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         setLat(position.coords.latitude);
+//         setLong(position.coords.longitude);
+//       });
+
+//       await fetch(`${url}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${key}`)
+//       .then(res => res.json())
+//       .then(result => {
+//         setData(result)
+//         console.log(result);
+//       });
+//     }
+//     fetchData();
+//   }, [lat,long])
+
+  // const fetechWeatherData = () => {
+  //   fetch(`${url}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${key}`)
+  //   .then(response=>{
+  //     return response.json()
+  //   })
+  //   .then(data => {
+  //     setData(data)
+  //   })
+  // }
+
+  // useEffect(() => {
+  //   fetechWeatherData()
+  // }, [])
---
+++ src/components/weather.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+
+// const CardExample = ({weatherData}) => (
+//     <Card>
+//         <Card.Content>
+//             <Card.Header className="header">{weatherData.name}</Card.Header>
+//                 <Card.Content>Feels like: {weatherData.main.feels_like}</Card.Content>
+//                 <Card.Content>temp: {weatherData.main.temp}</Card.Content>
+//                 <Card.Content>Max: {weatherData.main.temp_max}</Card.Content>
+//                 <Card.Content>Min: {weatherData.main.temp_min}</Card.Content>
+//                 <Card.Content>Humidity: {weatherData.main.humidity}</Card.Content>
+//                 <Card.Content>pressure: {weatherData.main.pressure}</Card.Content>
+//         </Card.Content>
+//     </Card>
+// )
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const current = new Date()
+let day = weekday[`${current.getDay()}`]
+let date = `${current.getDate()} ${current.getFullYear()}`
+
+const Weather = ({weatherData}, {locationData}) => (
+        <div className='widget'>
+        <div className='left-panel panel'>
+            <div className='date'>
+                <h1>{day} {date}</h1>
+            </div>
+            <div className='city'>
+                {/* <h2>{locationData.meta}</h2> */}
+            </div>
+            <div className='temp'>
+                <h2>{weatherData.main.temp}</h2>
+            </div>
+        </div>
+    </div>
+)
+
+export default Weather
